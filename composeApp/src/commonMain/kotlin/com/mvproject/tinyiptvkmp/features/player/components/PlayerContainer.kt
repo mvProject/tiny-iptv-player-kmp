dcb709100a588d19cc9a6f782d29330d74ba5f40
mvproject <awsom7@gmail.com>
2024-11-18 10:34:36 +0200

    prepare aspect ration update

--- composeApp/src/commonMain/kotlin/com/mvproject/tinyiptvkmp/features/player/components/PlayerContainer.kt
+++ composeApp/src/commonMain/kotlin/com/mvproject/tinyiptvkmp/features/player/components/PlayerContainer.kt
@@ -1,10 +1,11 @@
 package com.mvproject.tinyiptvkmp.features.player.components
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.mvproject.tinyiptvkmp.core.common.utils.PlayerUtils.adaptiveLayout
 import com.mvproject.tinyiptvkmp.features.player.PlayerUiAction
 import com.mvproject.tinyiptvkmp.features.player.PlayerUiState
 
@@ -15,18 +16,20 @@ fun PlayerContainer(
     onAction: (PlayerUiAction) -> Unit,
     toolbar: @Composable () -> Unit,
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .aspectRatio(uiState.videoRatio)
-        ) {
-            PlayerView(
-                uiState = uiState,
-                onAction = onAction
-            )
+    Box(modifier = modifier.fillMaxSize()) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            BoxWithConstraints(
+                modifier = Modifier
+                    .adaptiveLayout(
+                        aspectRatio = uiState.videoRatio,
+                        resizeMode = uiState.videoResizeMode,
+                    )
+            ) {
+                PlayerView(
+                    uiState = uiState,
+                    onAction = onAction
+                )
+            }
         }
 
         toolbar()
